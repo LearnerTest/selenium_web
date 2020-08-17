710334d4638954cc431e921c2d20dc223795b396
xTestLi <741297229@qq.com>
2020-08-17 14:34:30 +0800

    0811

--- src/main/java/com.imooc/SeleniumOne.java
+++ src/main/java/com.imooc/SeleniumOne.java
@@ -1,4 +1,24 @@
 package com.imooc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumOne {
+
+    WebDriver driver;
+
+    public void InitDriver() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("http://www.imooc.com");
+        Thread.sleep(3000);
+    }
+
+    public void GetElement(){
+        driver.get
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+
+    }
+
 }
